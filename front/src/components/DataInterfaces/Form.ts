ffceaf370a2db6a43016288c6869039476a583bf
AlBordun <125726038+AlBordun@users.noreply.github.com>
2023-12-26 16:51:42 +0300

    Add files via upload

---
+++ front/src/components/DataInterfaces/Form.ts
@@ -0,0 +1,12 @@
+export interface Form {
+
+    id : number | string | any;
+    date : string;
+    form_name: string;
+    inst_name: string;
+    profile_name: string;
+    route: string;
+    vmp_group: string;
+    patient_id: number | string | any;
+
+}
