0d8650bfd44b8f12785e42259a42c4e5bb96c477
AlBordun <125726038+AlBordun@users.noreply.github.com>
2023-12-27 07:47:04 +0300

    Delete back/MOKB_HOSPITAL_PROJECT-back/HTMS_MOKB directory

--- back/MOKB_HOSPITAL_PROJECT-back/HTMS_MOKB/src/main/java/ru/exceptions/InvalidRequestException.java
+++
@@ -1,9 +0,0 @@
-package ru.exceptions;
-
-public class InvalidRequestException extends RuntimeException {
-
-    public InvalidRequestException (String message){
-        super(message);
-    }
-
-}
