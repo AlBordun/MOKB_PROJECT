e72174e52c169796fc077b4d110bf707cc61bddd
AlBordun <125726038+AlBordun@users.noreply.github.com>
2024-01-03 08:18:28 +0300

    Add files via upload

---
+++ front/src/components/DataInterfaces/Patient.ts
@@ -0,0 +1,21 @@
+
+export interface Patient {
+    id: number | string | any;
+    [key: string]: any;
+    refType: string;
+    directionNumber: string;
+    populationCategory: string | number;
+    referralDate: string;
+    snils: string;
+    policyNumber: string;
+    lastName: string;
+    firstName: string;
+    middleName: string;
+    gender: string;
+    dateOfBirth: string;
+    documentType: string | number;
+    passportSeries: string;
+    passportNumber: string;
+    actualAddress: string;
+    registrationAddress: string;
+}
