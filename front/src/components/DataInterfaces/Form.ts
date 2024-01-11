d4ac8b348e09a7d10c9091cf2462632fd0b93493
AlBordun <125726038+AlBordun@users.noreply.github.com>
2024-01-11 16:54:29 +0300

    Add files via upload

---
+++ front/src/components/DataInterfaces/Form.ts
@@ -0,0 +1,22 @@
+export interface Form {
+
+    id : number | string | any;
+    date : string;
+    form_name: string;
+    inst_name: number | null;
+    profile_name: number;
+    route: string;
+    vmp_group: string;
+    disabled_cat: string | number | any;
+    need_cat: number;
+    payment_cat: number;
+    signature: number;
+    date_of_hosp: Date;
+    mkb_10: string | null ;
+    char_of_disease: number;
+    patient_model: string;
+    disability: number;
+    patient_id: number | string | any;
+
+
+}
---
+++ front/src/components/DataInterfaces/Institutions.ts
@@ -0,0 +1,13 @@
+export interface Institutions {
+
+    inst_id: number | string | any;
+
+    instName: number | string | any;
+
+    city: number;
+
+    instAddress: string;
+
+    fullInstName: string;
+
+}
