fdf25ece82f388194fa79cd8534b43c92c339419
AlBordun <125726038+AlBordun@users.noreply.github.com>
2024-01-15 08:08:11 +0300

    Add files via upload

---
+++ front/src/components/DataInterfaces/MKB10.ts
@@ -0,0 +1,9 @@
+export interface MKB10 {
+
+    mkb_id: number | string | any;
+
+    mkb_code: string;
+
+    mkb_name: string;
+
+}
---
+++ front/src/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import {BrowserRouter} from 'react-router-dom'
+import './App.css';
+import {DevSupport} from "@react-buddy/ide-toolbox";
+import {ComponentPreviews, useInitial} from "./dev";
+
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+);
+root.render(
+    <BrowserRouter>
+        <DevSupport ComponentPreviews={ComponentPreviews}
+                    useInitialHook={useInitial}
+        >
+            <App/>
+        </DevSupport>
+    </BrowserRouter>
+);
